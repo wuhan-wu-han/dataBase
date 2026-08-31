4777ba5e7a4878f7d8bda96a496f157bd12ee8f5
zhao <zyt20050403@qq.com>
2026-08-31 17:35:16 +0800

    chore: remove unused gas asset frontend folder

--- gas_asset_frontend/src/utils/chart.ts
+++
@@ -1,9 +0,0 @@
-/** 淡色系大屏图表配色 */
-export const PALETTE = ['#5b8ff9', '#5ad8a6', '#f6bd16', '#e86452', '#6dc8ec', '#945fb9', '#ff9845', '#269a99']
-
-/** 淡色坐标轴样式 */
-export const AXIS_STYLE = {
-  axisLine: { lineStyle: { color: '#c6d3e6' } },
-  axisLabel: { color: '#5a6b84' },
-  splitLine: { lineStyle: { color: '#eef2f8' } }
-}
